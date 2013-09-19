95742b0475cedff82ecd9c9072618f9bf27ccfea
andradeMartin <martin@wime.com.mx>
2013-09-19 17:43:15 -0500

    Modificacion indexView Encuentas

--- surveys/views.py
+++ surveys/views.py
@@ -6,7 +6,21 @@ from django.utils import simplejson
 
 
 def index(request):
-    surveys = Survey.objects.all().order_by('-date')
+    surveys = {'surveys': []}
+    survey_query = Survey.objects.all().order_by('-date')
+
+    for each_survey in survey_query:
+        counter_question = 0
+        for each_question in each_survey.questions.all():
+            counter_question += 1
+
+        surveys['surveys'].append(
+            {
+                "name": each_survey.name,
+                "date": each_survey.date,
+                "counter": counter_question
+            }
+        )
     template_vars = {"title": "Surveys",
                      "surveys": surveys}
     request_context = RequestContext(request, template_vars)
