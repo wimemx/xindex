fd46ed8ea51e3ae59345f41087f74df14712e04b
andradeMartin <martin@wime.com.mx>
2013-09-19 19:07:53 -0500

    Attributes_counter

--- surveys/views.py
+++ surveys/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.template.context import RequestContext
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from xindex.models import Survey
+from xindex.models import Survey, Question_Attributes
 from django.utils import simplejson
 
 
 def index(request):
     surveys = {'surveys': []}
     survey_query = Survey.objects.all().order_by('-date')
+    question_attribute_query = Question_Attributes.objects.all()
 
     for each_survey in survey_query:
         counter_question = 0
+        counter_attributes = 0
+
         for each_question in each_survey.questions.all():
             counter_question += 1
+
+        for each_question_attribute in question_attribute_query:
+            counter_attributes += 1
 
         surveys['surveys'].append(
             {
                 "name": each_survey.name,
                 "date": each_survey.date,
-                "counter": counter_question
+                "counter_question": counter_question
             }
         )
     template_vars = {"title": "Surveys",
